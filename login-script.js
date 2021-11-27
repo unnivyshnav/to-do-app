2d8ccc580d56144ac0dfa64bffa5106a735d205f
unnivyshnav <79002656+unnivyshnav@users.noreply.github.com>
2021-11-27 14:10:21 +0530

    to do background animated

--- login-script.js
+++ login-script.js
@@ -1,5 +1,3 @@
-$(document).ready(() => {
-    //  $(".btn").on("click", validate(redirect));
   
     $(".btn").click(() => {
       validate(redirect);
@@ -36,28 +34,5 @@ $(document).ready(() => {
         window.location.href = "./todolist.html";
       }
     }
-  });
   
-  // (e) => {
-  //   let uname = $(".uname");
-  //   let pass = $(".pass");
-  
-  //   if (uname[0].value != "admin") {
-  //     e.preventDefault();
-  //     uname.css("border", "solid red 2px");
-  //     $(".uname+.fas").css("visibility", "visible");
-  //   } else {
-  //     uname.css("border", "none");
-  //     $(".uname+.fas").css("visibility", "hidden");
-  //   }
-  
-  //   if (pass[0].value != 12345) {
-  //     e.preventDefault();
-  //     pass.css("border", "solid red 2px");
-  //     $(".pass+.fas").css("visibility", "visible");
-  //   } else {
-  //     pass.css("border", "none");
-  //     $(".pass+.fas").css("visibility", "hidden");
-  //   }
-  // };
   
